4659163807e774eaaafdfa4ade61a7e2bc6f09d1
Yago Juan <48865087+YagoYJ@users.noreply.github.com>
2020-03-27 18:19:14 -0300

    Rotas posts prontas para autenticação

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,21 @@ const { Router } = require("express");
 const routes = Router();
 
 // GET:
-
+// Tela de login:
 routes.get("/", (req, res) => {
   res.sendFile(__dirname + "/view/Login/login.html");
 });
 
+// Tela de cadastro de usuário:
 routes.get("/new-user", (req, res) => {
   res.sendFile(__dirname + "/view/New_User/newUser.html");
 });
+
+// POST:
+// Autenticação de login:
+routes.post("/", (req, res) => {});
+
+//Validação de cadastro de usuário:
+routes.post("/new-user", (req, res) => {});
 
 module.exports = routes;
