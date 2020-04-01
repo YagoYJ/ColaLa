b70ecd91b5481c5b2b01fa81c52bfca1c06a3027
Yago Juan <48865087+YagoYJ@users.noreply.github.com>
2020-04-01 10:04:26 -0300

    Organização de código

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,8 +1,32 @@
 const mongoose = require("mongoose");
+const joi = require("joi");
 require("../model/User");
 const User = mongoose.model("User");
 
 module.exports = {
   async index(req, res) {},
-  async create(req, res) {}
+  async create(req, res) {
+    console.log(req.body);
+
+    const schema = joi.object().keys({
+      user: joi
+        .string()
+        .trim()
+        .min(5)
+        .required(),
+      password: joi
+        .string()
+        .min(8)
+        .required()
+    });
+
+    joi.validate(req.body, schema, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.send("Usuário ou senha inválidos");
+      } else {
+        res.send("Login realizado com sucesso.");
+      }
+    });
+  }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,54 +1,20 @@
 const { Router } = require("express");
 const UserController = require("./controller/UserController");
 const SessionController = require("./controller/SessionController");
-const bodyParser = require("body-parser")
-const Joi = require('joi');
 
 const routes = Router();
 
-// Body Parser
-routes.use(bodyParser.urlencoded({ extended: false }))
-routes.use(bodyParser.json())
+// BACK-END:
 
-// Back-end:
-
-// GET:
 // Usuários:
 routes.get("/users", UserController.index);
+routes.post("/new-user", UserController.create);
 
-// POST:
-// Autenticação de login:
+// Login:
 routes.post("/session", SessionController.create);
 
-// Validação de login:
-routes.post("/login", (req, res) => {
-  console.log(req.body);
+// FRONT-END:
 
-  const schema = Joi.object().keys({
-    nickname: Joi.string().trim().min(5).required(),
-    password: Joi.string().min(8).required()
-  });
-
-  Joi.validate(req.body, schema, (err, result) =>{
-    if(err){
-      console.log(err)
-      return res.send("Usuário ou senha inválidos");
-    } else {
-      res.send('Login realizado com sucesso.');
-    }
-  })
-
-  // database code will go below
-
-
-});
-
-//Validação de cadastro de usuário:
-routes.post("/new-user", (req, res) => {});
-
-// Front-end:
-
-// GET
 // Tela de login:
 routes.get("/", (req, res) => {
   res.sendFile(__dirname + "/view/Login/login.html");
@@ -56,6 +22,7 @@ routes.get("/", (req, res) => {
 
 // Tela de cadastro de usuário:
 routes.get("/new-user", (req, res) => {
-  res.sendFile(__dirname + "/view/New_User/newUser.html");
+  res.sendFile(__dirname + "/view/uew_user/newUser.html");
 });
+
 module.exports = routes;
