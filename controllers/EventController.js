23b45a60014e94feb8d1a1a1b8269c97dd12514a
Yago Juan <48865087+YagoYJ@users.noreply.github.com>
2020-04-23 20:39:55 -0300

    Cadastro de eventos incompleto

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -9,7 +9,10 @@ const Modality = mongoose.model("Modality");
 module.exports = {
   create(req, res) {
     console.log(req.body);
-    req.flash("success_msg", "Evento cadastrado com sucesso:\n" + req.body);
+    req.flash(
+      "success_msg",
+      `Evento cadastrado com sucesso:\n Título: ${req.body.title}, Descrição: ${req.body.description}, Endereço: ${req.body.address}, Data: ${req.body.date}, Hora: ${req.body.hour}, Privado: ${req.body.private}`
+    );
     res.redirect("/home");
   },
 };
